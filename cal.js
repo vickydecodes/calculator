4e89b07f482766c4db42903c8f2f75c64fac1e65
vicky-at-web <144013983+vicky-at-web@users.noreply.github.com>
2023-12-22 11:13:27 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,29 @@
+const inps = document.querySelectorAll('#inp');
+const ans = document.querySelector('#ans');
+const eql = document.querySelector('#eql');
+const del = document.querySelector('#del');
+const clear = document.querySelector('#clear');
+const sec = document.querySelector('#section')
+
+for (let inp of inps) {
+    inp.addEventListener('click', function () {
+        ans.setAttribute('class', "col-span-4 text-center rounded-lg bg-sky-500/100 p-7 h-50 text-blue-600/100")
+
+        ans.innerText += inp.innerText;
+        console.log(inp.innerText);
+
+    })
+}
+
+eql.addEventListener('click', function () {
+    ans.innerText = eval(ans.innerText);
+})
+
+clear.addEventListener('click', function () {
+    ans.innerText = 'Answer';
+    ans.setAttribute('class', "col-span-4 text-center rounded-lg bg-sky-500/100 p-7 h-50 text-blue-600/25 ")
+})
+
+del.addEventListener('click', function () {
+    ans.innerText = ans.innerText.slice(0, -1);
+})
